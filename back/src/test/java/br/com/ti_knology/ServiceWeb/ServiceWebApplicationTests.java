e179db372a15598221670f53e0d45586f10b1c6a
AleixoCv <caleixodavi1811@gmail.com>
2024-11-08 18:20:27 -0300

    initial back-end push

---
+++ back/src/test/java/br/com/ti_knology/ServiceWeb/ServiceWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ti_knology.ServiceWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
